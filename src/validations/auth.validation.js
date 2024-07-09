7f8e81f6099037386ed9d540de1e2386027a7454
Rasheed <102271199+Demandtech@users.noreply.github.com>
2024-07-09 15:36:04 +0100

    Update auth.validation.js

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -4,21 +4,11 @@ export const registerSchema = Joi.object({
 	firstName: Joi.string().required(),
 	lastName: Joi.string().required(),
 	email: Joi.string().email().required(),
-	password: Joi.string()
-		.min(8)
-		.required()
-		.pattern(
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':"\\|,.<>?])[A-Za-z\d!@#$%^&*()_+\[\]{};':"\\|,.<>?]{8,}$/,
-			"password requirements"
-		)
-		.messages({
-			"string.pattern.base":
-				"Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.",
-		}),
+	password: Joi.string().required(),
 	phone: Joi.string(),
 });
 
 export const loginSchema = Joi.object({
 	email: Joi.string().email().required(),
-	password: Joi.string().min(8).required(),
+	password: Joi.string().required(),
 });
